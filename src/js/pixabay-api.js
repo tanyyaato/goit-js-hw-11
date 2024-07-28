b8d206f0c1a1ad057fcd109b43b0124d13664f57
Tetiana Tolmachova <totatiana2015@gmail.com>
2024-07-28 17:17:50 +0200

    hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,25 @@
+'use sctict';
+export function searchPhotoByQuery(query) {
+  const URL = 'https://pixabay.com/api/';
+  const API_KEY = '45156529-f4caada2c6812495035285f69';
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  return fetch(`${URL}?${searchParams.toString()}`, {
+    headers: {
+      Accept: 'application/json',
+    },
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+searchPhotoByQuery('yellow')
+  .then(data => console.log(data))
+  .catch(console.log);
---
+++ src/js/render-function.js
@@ -0,0 +1,4 @@
+'use strict';
+const form = document.querySelector('.js-form');
+const picturesList = document.querySelector('.list');
+import { searchPhotoByQuery } from './pixabay-api.js';
