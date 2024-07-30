9cb237c183d02de3afa5277ad4ed31105dd69deb
Tetiana Tolmachova <totatiana2015@gmail.com>
2024-07-30 16:30:46 +0200

    hw

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -1,37 +1,11 @@
 'use strict';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-const form = document.querySelector('.js-form');
 const picturesList = document.querySelector('.list');
-const textDuringLoad = document.querySelector('.js-loading-text');
-import { searchPhotoByQuery } from './pixabay-api.js';
-form.addEventListener('submit', searchQuery);
-//
-function searchQuery(event) {
-  event.preventDefault();
-  const form = event.currentTarget;
-  const inputValue = form.elements.request.value.trim().toLowerCase();
-  if (inputValue === '') {
-    return;
-  }
-  textDuringLoad.textContent = 'Loading images, please wait...';
-  searchPhotoByQuery(inputValue)
-    .then(data => {
-      if (data.hits.length === 0) {
-        throw new Error(response.status);
-      }
-      textDuringLoad.style.display = 'none';
-      markUpRequest(data.hits);
-    })
-    .catch(error => {
-      textDuringLoad.style.display = 'none';
-      catchError(error);
-    })
-    .finally(form.reset());
-}
-
-function markUpRequest(hits) {
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const lightbox = new SimpleLightbox('.list  a', {
+  captionsData: 'alt',
+});
+export function markUpRequest(hits) {
   picturesList.innerHTML = '';
   const markUp = hits
     .map(
@@ -62,24 +36,5 @@ function markUpRequest(hits) {
     )
     .join('');
   picturesList.innerHTML = markUp;
-}
-function catchError(error) {
-  return iziToast.show({
-    message:
-      'Sorry, there are no images matching your search query. Please try again!',
-    color: 'red',
-    position: 'topRight',
-    closeOnClick: true,
-  });
-}
-picturesList.addEventListener('click', openModalWindow);
-
-function openModalWindow(event) {
-  event.preventDefault();
-  if (event.target === event.currentTarget) {
-    return;
-  }
-  const lightbox = new SimpleLightbox('.list  a', {
-    captionData: 'alt',
-  });
+  lightbox.refresh();
 }
